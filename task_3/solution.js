8e0311d2121e57593af5d1ac25434d001d5449f6
Neto test <altyvai@yandex.ru>
2023-09-21 00:29:17 +0700

    hw3

--- task_3/solution.js
+++ task_3/solution.js
@@ -2,14 +2,21 @@ function renderCartItem(item) {
     const product = item;
 
     // Задание №3.1. Формирование строки корзины
-
+    
     // product - объект вида {id: id, title: title, count: count, price: price}
     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
     // id - идентификатор
     // title - наименование
     // price - цена
     // count - количество
-
+    let itemCountText; 
+    itemCountText = itemCountText*count;
+    itemCountText = itemCountText+` × `;
+    itemCountText = itemCountText+price;
+    itemCountText = itemCountText+` ₽ = `;
+    sum = count * price;
+    itemCountText = itemCountText+sum;
+    itemCountText = itemCountText+` ₽ = `;
     // сформируйте строку itemCountText вида:
     // количество × цена ₽ = стоимость ₽
     // например,
