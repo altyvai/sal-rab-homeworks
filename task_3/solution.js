b14e5be89a9312ecdf08330541aae0365fec05b6
Neto test <altyvai@yandex.ru>
2023-09-21 00:44:46 +0700

    he3

--- task_3/solution.js
+++ task_3/solution.js
@@ -16,7 +16,7 @@ function renderCartItem(item) {
     itemCountText = itemCountText + ` ₽ = `;
     let sum = product.count * product.price;
     itemCountText = itemCountText + sum;
-    itemCountText = itemCountText + ` ₽ `;
+    itemCountText = itemCountText + ` ₽`;
     // сформируйте строку itemCountText вида:
     // количество × цена ₽ = стоимость ₽
     // например,
