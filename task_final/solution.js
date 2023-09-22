f2ce66a4c09029593a31ed0d9f652feb5adc580f
Neto test <altyvai@yandex.ru>
2023-09-23 00:57:14 +0700

    it5

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,12 +34,19 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {data: {client: name + " " + phone, order: {address, sum}, goods: []}};
+    data.data.client = name + " " + phone;
+    data.data.order.address = "ул. " + address.street + ", " + 
+                          "дом " + address.house + ", " +
+                          address.entrance + " подъезд, " +
+                          address.floor + " этаж, кв. " +
+                          address.flat;
+    data.data.order.sum = sum;
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
     data.order.address = address;
